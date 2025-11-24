3530ad6180d74dc00c908539c1f2cd033403b94a
Najiya Irawan <najiyairawan06@gmail.com>
2025-11-24 14:27:19 +0700

    Update EditSiswa.java

--- actions/EditSiswa.java
+++ actions/EditSiswa.java
@@ -1,1 +1,26 @@
+package actions;
 
+import main.Aplikasi;
+import model.Siswa;
+import model.User;
+import util.DataManager;
+
+public class EditSiswa {
+    public void eksekusi() {
+        // Tampilkan daftar dulu biar gampang milih
+        new LihatSiswa().eksekusi(); 
+        
+        System.out.println("\n--- EDIT DATA SISWA ---");
+        System.out.print("Masukkan Username siswa yang akan diedit: ");
+        String targetUser = Aplikasi.scanner.nextLine();
+        
+        Siswa found = null;
+        for (User u : Aplikasi.userList) {
+            if (u instanceof Siswa && u.getUsername().equals(targetUser)) {
+                found = (Siswa) u;
+                break;
+            }
+        }
+
+
+      
