d32adfcacebab23dd8bf18d74b8d538270b392e9
Najiya Irawan <najiyairawan06@gmail.com>
2025-11-24 14:28:48 +0700

    Add HapusSiswa class for deleting student users
    Implement user deletion functionality for Siswa.

--- actions/HapusSiswa.java
+++ actions/HapusSiswa.java
@@ -1,1 +1,23 @@
+package actions;
 
+import main.Aplikasi;
+import model.Siswa;
+import model.User;
+import util.DataManager;
+
+public class HapusSiswa {
+    public void eksekusi() {
+        // Panggil LihatSiswa dulu biar admin tau siapa yg mau dihapus
+        new LihatSiswa().eksekusi(); 
+        
+        System.out.print("Username siswa yang akan dihapus: ");
+        String targetUser = Aplikasi.scanner.nextLine();
+        
+        User target = null;
+        for (User u : Aplikasi.userList) {
+            if (u instanceof Siswa && u.getUsername().equals(targetUser)) {
+                target = u;
+                break;
+            }
+        }
+      
